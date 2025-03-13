f9da92de91b0d06f92a33f74355f32bf92fedd71
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-03-13 17:29:52 +0700

    Update [id].js

--- pages/api/sessions2/[id].js
+++ pages/api/sessions2/[id].js
@@ -1,5 +1,5 @@
-// pages/api/sessions/[id].js
-const { sessions } = require("../../../data");
+// pages/api/sessions2/[id].js
+const { getSessions, saveData, data } = require("../../../data");
 
 export default function handler(req, res) {
   const { id } = req.query;
@@ -8,10 +8,11 @@ export default function handler(req, res) {
     if (authorization !== "Bearer dev123") {
       return res.status(403).json({ error: "Unauthorized" });
     }
-    if (!sessions[id]) {
+    if (!data.sessions[id]) {
       return res.status(404).json({ error: "Session not found" });
     }
-    delete sessions[id];
+    delete data.sessions[id];
+    saveData();
     res.status(200).json({ message: "Session deleted" });
   } else {
     res.status(405).json({ error: "Method not allowed" });
