27494e02c3bf1591d5e1b8bedd151ce163301d91
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-03-13 17:30:18 +0700

    Update names.js

--- pages/api/names.js
+++ pages/api/names.js
@@ -1,23 +1,23 @@
 // pages/api/names.js
-const { sessions } = require("../../data");
+const { getSessions, saveData, data } = require("../../data");
 
 export default function handler(req, res) {
   if (req.method === "GET") {
-    // Mengambil sessionId dari query string
     const { sessionId } = req.query;
-    if (!sessionId || !sessions[sessionId]) {
+    if (!sessionId || !data.sessions[sessionId]) {
       return res.status(404).json({ error: "Session not found" });
     }
-    res.status(200).json(sessions[sessionId].names);
+    res.status(200).json(data.sessions[sessionId].names);
   } else if (req.method === "POST") {
     const { sessionId, name } = req.body;
-    if (!sessionId || !sessions[sessionId]) {
+    if (!sessionId || !data.sessions[sessionId]) {
       return res.status(404).json({ error: "Session not found" });
     }
     if (!name) {
       return res.status(400).json({ error: "Name is required" });
     }
-    sessions[sessionId].names.push(name);
+    data.sessions[sessionId].names.push(name);
+    saveData();
     res.status(200).json({ message: "Name added" });
   } else {
     res.status(405).json({ error: "Method not allowed" });
