e5d2c096650183673794ecfee31264787cbea7c9
TensuraBot <165671956+TensuraBot@users.noreply.github.com>
2025-03-13 17:32:04 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,30 @@
 // data.js
-const sessions = {
-  default: { id: "default", names: [] },
-};
+const fs = require("fs");
+const path = require("path");
 
-module.exports = { sessions };
+const dataFilePath = path.join(process.cwd(), "data.json");
+
+function readData() {
+  try {
+    const data = fs.readFileSync(dataFilePath, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    return { sessions: { default: { id: "default", names: [] } } };
+  }
+}
+
+function writeData(data) {
+  fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+}
+
+let data = readData();
+
+function getSessions() {
+  return data.sessions;
+}
+
+function saveData() {
+  writeData(data);
+}
+
+module.exports = { getSessions, saveData, data };
